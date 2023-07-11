359d2548fefa2ae6a00391a4f0aee434cfa6d344
liuqinh2s <liuqinh2s@gmail.com>
2023-07-11 02:02:32 +0000

    首页显示不出来的bug

--- src/components/EBook/EBook.tsx
+++ src/components/EBook/EBook.tsx
@@ -8,15 +8,15 @@ interface IEBook {
 export const EBook = (props: IEBook) => {
   const { source } = props;
   // And your own state logic to persist state
-  const [location, setLocation] = useState('');
-  const locationChanged = (epubcifi: string) => {
+  const [location, setLocation] = useState(null);
+  const locationChanged = (epubcifi: any) => {
     // epubcifi is a internal string used by epubjs to point to a location in an epub. It looks like this: epubcfi(/6/6[titlepage]!/4/2/12[pgepubid00003]/3:0)
     setLocation(epubcifi);
   };
   return (
     <div style={{ height: "100vh" }}>
       <ReactReader
-        location={location}
+        location={location as any}
         locationChanged={locationChanged}
         url={source}
       />
