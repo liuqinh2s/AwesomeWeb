98e9352e30941b334f91162e56ba79469ff321bd
liuqinh2s <liuqinh2s@gmail.com>
2025-09-19 19:55:54 +0800

    修复route

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,19 @@ import { BookPage } from "./views/BookPage/BookPage";
 import { HomePage } from "./views/HomePage/HomePage";
 import { MusicPage } from "./views/MusicPage/MusicPage";
 import { GamePage } from "./views/GamePage/GamePage";
+import { ENV_PATH } from "./config";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage></HomePage>} />
-          <Route path="/:id" element={<HomePage></HomePage>} />
-          <Route path="/video/:id" element={<VideoPage></VideoPage>} />
-          <Route path="/ebook/:id" element={<BookPage></BookPage>} />
-          <Route path="/music" element={<MusicPage></MusicPage>} />
-          <Route path="/game/:id" element={<GamePage></GamePage>} />
+          <Route path={ENV_PATH+"/"} element={<HomePage></HomePage>} />
+          <Route path={ENV_PATH+"/:id"} element={<HomePage></HomePage>} />
+          <Route path={ENV_PATH+"/video/:id"} element={<VideoPage></VideoPage>} />
+          <Route path={ENV_PATH+"/ebook/:id"} element={<BookPage></BookPage>} />
+          <Route path={ENV_PATH+"/music"} element={<MusicPage></MusicPage>} />
+          <Route path={ENV_PATH+"/game/:id"} element={<GamePage></GamePage>} />
         </Routes>
         {/* <VideoCard data={video[0]}></VideoCard> */}
         {/* <SimpleCard name={'刻在你心底的名字'} cover={"https://pic.rmb.bdstatic.com/bjh/4092b755ca751ae195a4f6635a041843.jpeg"} score={60}></SimpleCard> */}
