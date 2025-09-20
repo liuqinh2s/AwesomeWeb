020bd2862f2b8c704e046a351f06508b525eed68
liuqinh2s <liuqinh2s@gmail.com>
2025-09-20 12:49:08 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import commonjs from 'vite-plugin-commonjs';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),commonjs()],
-  base: "./",
+  base: "./AwesomeWeb",
   server: {
     host: "0.0.0.0",
   },
